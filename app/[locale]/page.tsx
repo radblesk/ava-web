4696ba164bfff75577f6015f2e6ed3a603dce779
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-17 18:21:36 +0100

    feat(homepage): many design and layout improvements
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,37 +3,30 @@ import CompatibilityCard from "@/components/compatibilityCard";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
+// Images Imports
+import iconDark from "@/public/icon-dark.webp";
+import iconLight from "@/public/icon-light.webp";
+import footerLogo from "@/public/wide-dark.svg";
+
 export default function Home() {
   const t = useTranslations("homepage");
 
   return (
-    <div className="flex min-h-screen items-center justify-center overflow-y-auto bg-zinc-100 font-sans select-none dark:bg-zinc-950">
-      <main className="relative flex h-svh min-h-fit w-full max-w-3xl flex-col items-center justify-between gap-10 px-16 py-12 sm:justify-center sm:gap-20 lg:py-32">
+    <div className="flex min-h-screen items-center justify-center overflow-y-auto">
+      <main className="relative flex h-svh min-h-fit w-full max-w-3xl flex-col items-center justify-between gap-10 px-16 py-12 sm:justify-center sm:gap-20 lg:py-22">
         <Image
-          className="fixed inset-6 z-20 mix-blend-difference invert lg:top-6 lg:left-1/3"
-          src="/monogram-dark.svg"
+          className="z-10 block h-24 w-auto rounded-3xl shadow-2xl shadow-sky-800/50 motion-safe:animate-[bounce_3s_ease-in-out_infinite] dark:hidden"
+          src={iconLight}
           alt="Ava logo"
-          width={48}
-          height={48}
           priority
         />
         <Image
-          className="z-10 block dark:hidden"
-          src="/icon-light.png"
+          className="animate-2 z-10 hidden h-24 w-auto rounded-3xl shadow-2xl shadow-sky-600/30 motion-safe:animate-[bounce_3s_ease-in-out_infinite] dark:block"
+          src={iconDark}
           alt="Ava logo"
-          width={100}
-          height={100}
           priority
         />
-        <Image
-          className="z-10 hidden dark:block"
-          src="/icon-dark.png"
-          alt="Ava logo"
-          width={100}
-          height={100}
-          priority
-        />
-        <div className="z-10 flex flex-col items-center gap-6 text-center">
+        <div className="z-10 flex flex-col items-center gap-6 text-center drop-shadow-lg drop-shadow-black/20">
           <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
             {t("headline")}
           </h1>
@@ -44,7 +37,7 @@ export default function Home() {
 
         <div className="flex flex-col gap-2">
           <p className="text-zinc-400">{t("compatibility.headline")}</p>
-          <div className="relative flex gap-2">
+          <div className="relative flex flex-wrap items-center justify-center gap-4 sm:gap-6">
             <CompatibilityCard
               symbol="iphone11"
               alt="iPhone 11 symbol"
@@ -57,6 +50,18 @@ export default function Home() {
             />
             <CompatibilityCard
               symbol="watch6"
+              alt="Apple Watch Series 6 symbol"
+              size={76}
+              onLaunch={false}
+            />
+            <CompatibilityCard
+              symbol="macbook"
+              alt="Apple Watch Series 6 symbol"
+              size={76}
+              onLaunch={false}
+            />
+            <CompatibilityCard
+              symbol="vision.pro"
               alt="Apple Watch Series 6 symbol"
               size={76}
               onLaunch={false}
@@ -81,7 +86,6 @@ export default function Home() {
             url="https://github.com/radblesk/"
             symbol="github.svg"
             alt="GitHub logo"
-            disabled={true}
           />
           <Button
             title="r/AvaApp"
@@ -98,29 +102,28 @@ export default function Home() {
           />
         </div>
 
-        <div className="border-foreground/20 w-full border-t"></div>
+        <div className="border-foreground w-full border-t mask-x-from-0"></div>
 
-        <div className="flex flex-col items-center gap-12 text-center">
-          <div className="flex gap-2">
-            <p>&copy; {new Date().getFullYear()}</p>
+        <div className="flex flex-col items-center gap-8 text-center text-xs text-zinc-600 dark:text-zinc-400">
+          <p>{t("footer.reserved")}</p>
+
+          <p>{t("footer.appleTrademark")}</p>
+
+          <div className="flex items-center justify-center">
+            <p className="text-sm">&copy; {new Date().getFullYear()}</p>
             <a
               href="https://www.radobley.sk"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Image
-                className="dark:invert"
-                src="/wide-dark.svg"
-                alt="Next.js logo"
-                width={160}
-                height={50}
+                className="ml-3 h-4 w-auto border-l border-zinc-600 pl-3 dark:border-zinc-400 dark:invert"
+                src={footerLogo}
+                alt="Radoslav Bley wide logo"
                 priority
               />
             </a>
           </div>
-          <p className="text-xs text-zinc-600 dark:text-zinc-400">
-            {t("footer.appleTrademark")}
-          </p>
         </div>
       </main>
     </div>
