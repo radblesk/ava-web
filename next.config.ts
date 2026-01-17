701c61ac10f13140db15d0e4e5e5dcc52f086a14
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-17 12:35:43 +0100

    chore(cloudflare): removed cloudflare support
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,7 +8,3 @@ const nextConfig: NextConfig = {
 const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-import createNextIntlPlugin from "next-intl/plugin";
-initOpenNextCloudflareForDev();
