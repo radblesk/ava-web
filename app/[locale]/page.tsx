e9b368e11a1ebd2b5faba4be4e0a62fc6e3458db
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-17 15:53:27 +0100

    feat(pages): created fully localized home page
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,66 +1,126 @@
+import Button from "@/components/button";
+import CompatibilityCard from "@/components/compatibilityCard";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function Home() {
   const t = useTranslations("homepage");
-  
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen select-none overflow-y-auto items-center justify-center bg-zinc-100 font-sans dark:bg-zinc-950">
+      <main className="flex relative h-svh min-h-fit w-full max-w-3xl flex-col items-center justify-between sm:justify-center sm:gap-20 gap-10 py-12 lg:py-32 px-16">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          className="dark:invert fixed inset-6 lg:top-6 lg:left-1/3 z-20 mix-blend-difference"
+          src="/monogram-dark.svg"
+          alt="Ava logo"
+          width={48}
+          height={48}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <Image
+          className="dark:hidden block z-10"
+          src="/icon-light.png"
+          alt="Ava logo"
+          width={100}
+          height={100}
+          priority
+        />
+        <Image
+          className="dark:block hidden z-10"
+          src="/icon-dark.png"
+          alt="Ava logo"
+          width={100}
+          height={100}
+          priority
+        />
+        <div className="flex z-10 flex-col items-center gap-6 text-center">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            {t("headline")}
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-md text-lg leading-6 sm:leading-7 text-zinc-600 dark:text-zinc-400">
+            {t("subheadline")}
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+
+        <div className="flex flex-col gap-2">
+          <p className="text-zinc-400">{t("compatibility.headline")}</p>
+          <div className="flex relative gap-2">
+            <CompatibilityCard
+              symbol="iphone11"
+              alt="iPhone 11 symbol"
+              size={30}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <CompatibilityCard
+              symbol="ipadmini"
+              alt="iPad Mini (5th generation) symbol"
+              size={34}
+            />
+            <CompatibilityCard
+              symbol="watch6"
+              alt="Apple Watch Series 6 symbol"
+              size={76}
+              onLaunch={false}
+            />
+          </div>
+
+          <p className="text-zinc-400 pt-2 text-xs">
+            *{t("compatibility.footer")}
+          </p>
+        </div>
+
+        <div className="flex z-10 justify-center flex-wrap max-w-sm sm:max-w-lg gap-4 text-base font-medium">
+          <Button
+            title="TestFlight"
+            url=""
+            symbol="fan.svg"
+            alt="TestFlight alternative logo"
+            disabled={true}
+          />
+          <Button
+            title="GitHub"
+            url="https://github.com/radblesk/"
+            symbol="github.svg"
+            alt="GitHub logo"
+            disabled={true}
+          />
+          <Button
+            title="r/AvaApp"
+            url="https://www.reddit.com/r/AvaApp"
+            symbol="reddit.svg"
+            alt="Reddit logo"
+            disabled={true}
+          />
+          <Button
+            title={t("buttons.developer")}
+            url="https://www.radobley.sk"
+            symbol="monogram-dark.svg"
+            alt="Radoslav Bley logo"
+          />
+        </div>
+
+        <div className="w-full border-t border-foreground/20"></div>
+
+        <div className="flex flex-col items-center text-center gap-12">
+          <div className="flex gap-2">
+            <p>&copy; {new Date().getFullYear()}</p>
+            <a
+              href="https://www.radobley.sk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className="dark:invert"
+                src="/wide-dark.svg"
+                alt="Next.js logo"
+                width={160}
+                height={50}
+                priority
+              />
+            </a>
+          </div>
+          <p className="text-xs text-zinc-600 dark:text-zinc-400">
+            {t("footer.appleTrademark")}
+          </p>
         </div>
       </main>
     </div>
