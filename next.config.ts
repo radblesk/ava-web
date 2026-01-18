191b3df25e926de19af79caaa3494b8014740182
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-18 15:14:26 +0100

    feat: image optimizations
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.1.19"],
+  images: {
+    formats: ["image/avif", "image/webp"],
+    qualities: [75],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
