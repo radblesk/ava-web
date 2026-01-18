809c2accc622b982c89a76230b6005ee7fc80151
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-19 00:07:12 +0100

    feat(homepage): improved layout and design
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,35 @@
-import Button from "@/components/button";
-import CompatibilityCard from "@/components/compatibilityCard";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+
+// Components Imports
+import Button from "@/components/button";
+import CompatibilityCard from "@/components/compatibilityCard";
+import SectionHeader from "@/components/SectionHeader";
+import FeatureBlock1 from "@/components/FeatureBlock1";
+import FeatureBlock2 from "@/components/FeatureBlock2";
 
 // Images Imports
 import iconDark from "@/public/icon-dark.webp";
 import iconLight from "@/public/icon-light.webp";
 import footerLogo from "@/public/wide-dark.svg";
+import rays from "@/public/rays.webp";
 
 export default function Home() {
   // Translations
   const t = useTranslations("homepage");
+
+  // Headers
+  const headers = [
+    {
+      text: t("headers.features"),
+    },
+    {
+      text: t("headers.resources"),
+    },
+    {
+      text: t("headers.footer"),
+    },
+  ];
 
   // Compatibility Cards Data
   const compatibilityCards = [
@@ -38,32 +57,41 @@ export default function Home() {
   const buttonKeys = Object.keys(buttons);
 
   return (
-    <div className="flex min-h-screen items-center justify-center overflow-y-auto dark:bg-linear-to-b dark:from-gray-950 dark:to-zinc-950">
-      <main className="relative flex h-svh min-h-fit w-full max-w-4xl flex-col items-center justify-between gap-10 px-10 py-12 sm:justify-center sm:gap-20 sm:px-16 lg:py-22">
+    <div className="relative flex min-h-screen items-center justify-center overflow-y-auto bg-radial-[at_50%_5%] from-[#D1E1EC]/45 to-zinc-100 to-70% sm:to-50% dark:from-[#00101C] dark:to-zinc-950">
+      {/* Overlay */}
+      <Image
+        src={rays}
+        alt="Light Rays overlay"
+        className="absolute inset-0 h-screen w-screen mask-b-from-0 object-cover opacity-70 mix-blend-screen dark:opacity-5"
+      />
+
+      <main className="relative flex h-svh min-h-fit w-full max-w-4xl flex-col items-center justify-center gap-10 px-10 py-12 sm:gap-20 sm:px-16 lg:py-22">
         {/* Animated Ava Icon */}
         <Image
-          className="z-10 block h-24 w-auto rounded-3xl shadow-2xl shadow-sky-800/50 motion-safe:animate-[bounce_3s_ease-in-out_infinite] dark:hidden"
+          className="z-10 block h-24 w-auto rounded-3xl shadow-2xl shadow-sky-800/50 dark:hidden"
           src={iconLight}
           alt="Ava Icon Light"
           priority
         />
         <Image
-          className="animate-2 z-10 hidden h-24 w-auto rounded-3xl shadow-2xl shadow-sky-500/30 motion-safe:animate-[bounce_3s_ease-in-out_infinite] dark:block"
+          className="animate-2 z-10 hidden h-24 w-auto rounded-3xl shadow-2xl shadow-sky-500/30 dark:block"
           src={iconDark}
           alt="Ava Icon Dark"
           priority
         />
 
         {/* Hero */}
-        <div className="z-10 flex flex-col items-center gap-6 text-center drop-shadow-lg drop-shadow-black/20">
+        <div className="z-10 flex flex-col items-center gap-6 text-center">
           <h1 className="flex max-w-xs gap-2 text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
             {t.rich("hero.headline", {
               super: (chunks) => (
-                <span className="text-base text-orange-500">{chunks}</span>
+                <span className="text-base text-orange-600 dark:text-orange-500">
+                  {chunks}
+                </span>
               ),
             })}
           </h1>
-          <p className="max-w-md text-lg leading-6 text-balance text-zinc-600 sm:leading-7 dark:text-zinc-400">
+          <p className="max-w-lg text-lg leading-6 text-balance text-zinc-600 sm:leading-7 dark:text-zinc-400">
             {t.rich("hero.subheadline", {
               highlighted: (chunks) => (
                 <span className="font-medium text-zinc-950 dark:text-zinc-50">
@@ -75,8 +103,8 @@ export default function Home() {
         </div>
 
         {/* Compatibility Cards */}
-        <div className="flex flex-col gap-2">
-          <p className="font-medium tracking-tight text-zinc-600 dark:text-zinc-400">
+        <div className="flex flex-col justify-center gap-6 text-center">
+          <p className="font-semibold tracking-tight text-zinc-600 dark:text-zinc-400">
             {t("compatibility.headline")}
           </p>
           <div className="relative flex flex-wrap items-center justify-center gap-4 sm:gap-6">
@@ -95,19 +123,39 @@ export default function Home() {
           </p>
         </div>
 
+        {/* Divider */}
+        <div className="bg-foreground my-16 h-px w-full mask-x-from-0"></div>
+
+        {/* Features */}
+        <div className="relative flex max-w-screen flex-wrap items-center justify-center gap-16">
+          <SectionHeader text={headers[0].text ?? ""} />
+
+          <FeatureBlock1 />
+          <FeatureBlock2 />
+        </div>
+
+        {/* Divider */}
+        <div className="bg-foreground my-16 h-px w-full mask-x-from-0"></div>
+
         {/* Notice */}
-        <p className="max-w-md text-center text-sm text-zinc-600 dark:text-zinc-400">
-          {t.rich("notice", {
-            bold: (chunks) => (
-              <span className="font-medium text-zinc-950 dark:text-zinc-50">
-                {chunks}
-              </span>
-            ),
-            highlighted: (chunks) => (
-              <span className="font-medium text-orange-500">{chunks}</span>
-            ),
-          })}
-        </p>
+        <div className="relative flex w-full justify-center">
+          <SectionHeader text={headers[1].text ?? ""} />
+
+          <p className="relative max-w-md text-center text-base text-zinc-600 dark:text-zinc-400">
+            {t.rich("notice", {
+              bold: (chunks) => (
+                <span className="font-medium text-zinc-950 dark:text-zinc-50">
+                  {chunks}
+                </span>
+              ),
+              highlighted: (chunks) => (
+                <span className="font-medium text-orange-600 dark:text-orange-500">
+                  {chunks}
+                </span>
+              ),
+            })}
+          </p>
+        </div>
 
         {/* Buttons */}
         <div className="z-10 flex max-w-sm flex-wrap gap-4 text-base font-medium sm:max-w-lg">
@@ -124,29 +172,32 @@ export default function Home() {
         </div>
 
         {/* Divider */}
-        <div className="bg-foreground h-px w-full mask-x-from-0"></div>
+        <div className="bg-foreground my-16 h-px w-full mask-x-from-0"></div>
 
         {/* Footer */}
-        <div className="flex flex-col items-center gap-8 text-center text-xs text-zinc-600 dark:text-zinc-400">
-          <p>{t("footer.reserved")}</p>
+        <div className="relative flex max-w-md flex-col items-center gap-8 text-center text-xs text-zinc-600 dark:text-zinc-400">
+          <SectionHeader text={headers[2].text ?? ""} />
+          <p>{t("footer.affiliate")}</p>
 
           <p>{t("footer.appleTrademark")}</p>
 
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center gap-4">
             <p className="text-sm">&copy; {new Date().getFullYear()}</p>
+            <div className="h-4 w-px -skew-12 bg-orange-600 dark:bg-orange-500"></div>
             <a
               href="https://www.radobley.sk"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Image
-                className="ml-3 h-4 w-auto border-l border-zinc-600 pl-3 dark:border-zinc-400 dark:invert"
+                className="h-4 w-auto dark:invert"
                 src={footerLogo}
                 alt="Radoslav Bley wide logo"
                 priority
               />
             </a>
           </div>
+          <p>{t("footer.reserved")}</p>
         </div>
       </main>
     </div>
