7696f90cbedc58f03b8fe4ab20e0eab8f2f1d4fa
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-19 00:05:32 +0100

    feat(components): added SectionHeader component
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

---
+++ components/SectionHeader.tsx
@@ -0,0 +1,14 @@
+// Types
+type Props = {
+  text: string;
+};
+
+export default function SectionHeader({ text }: Props) {
+  return (
+    <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-26 mask-x-from-85% opacity-10 sm:-translate-y-36">
+      <h2 className="noise bg-clip-text text-5xl font-black text-transparent uppercase sm:text-7xl">
+        {text}
+      </h2>
+    </div>
+  );
+}
