8aaf93b05449fc735201113da62808fe4583c31f
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-18 17:30:37 +0100

    feat(components): redesigned buttons
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- components/button.tsx
+++ components/button.tsx
@@ -18,15 +18,15 @@ export default function Button({
 }: Props) {
   return (
     <a
-      className={`group border-foreground/10 bg-foreground/75 text-background flex h-12 w-full items-center justify-center gap-2 overflow-hidden rounded-full border px-5 transition will-change-transform md:w-39.5 ${disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer hover:bg-sky-400 hover:text-black motion-safe:hover:scale-105 dark:hover:bg-sky-600 dark:hover:text-white"}`}
+      className={`group border-foreground/10 bg-foreground/85 text-background hover:bg-foreground/65 flex h-12 w-full items-center justify-center gap-4 overflow-hidden rounded-full border px-5 transition duration-300 sm:flex-1/3 md:w-39.5 ${disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer"}`}
       href={disabled ? undefined : url}
       aria-disabled={disabled}
       target="_blank"
       rel="noopener noreferrer"
     >
       <Image
-        className={`invert transition dark:invert-0 ${disabled ? "opacity-50" : "group-hover:invert-0 dark:group-hover:invert-100"}`}
-        src={symbol}
+        className={`invert transition dark:invert-0 ${disabled ? "opacity-50" : ""}`}
+        src={`${symbol}.svg`}
         alt={alt}
         width={16}
         height={16}
