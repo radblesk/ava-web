d80cd90bf0b2a9207b439dccd6320160f129643c
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-18 17:30:03 +0100

    feat(components): improved navbar logo visibility
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,14 +11,14 @@ export default function Navbar() {
     <nav className="sticky top-0 z-50 flex h-14 w-full items-center justify-center border-b border-zinc-200 bg-zinc-100/75 py-3.5 backdrop-blur-lg sm:h-12 dark:border-zinc-800 dark:bg-zinc-950/75">
       <div className="relative flex h-full w-full max-w-7xl items-center justify-between">
         <Image
-          className="h-full w-auto mix-blend-difference invert lg:top-6 lg:left-1/4"
+          className="h-full w-auto lg:top-6 lg:left-1/4 dark:invert"
           src={rbLogo}
           alt="Radoslav Bley logo"
           priority
         />
 
         <p className="dark:zinc-400 text-sm font-extralight text-zinc-600">
-          {t("buttons.inDev")}
+          {t("navbar.inDev")}
         </p>
       </div>
     </nav>
