4b64de069eaf28d3c3811b815b1e7019838254b6
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-18 17:53:43 +0100

    feat(localizations): updated translations + added polish localization
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,20 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["cs", "de", "en", "es", "fr", "ja", "ko", "pt", "ru", "sk", "zh"],
+  locales: [
+    "cs",
+    "de",
+    "en",
+    "es",
+    "fr",
+    "ja",
+    "ko",
+    "pl",
+    "pt",
+    "ru",
+    "sk",
+    "zh",
+  ],
 
   // Used when no locale matches
   defaultLocale: "en",
