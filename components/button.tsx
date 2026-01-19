54de9993a1a7d7f584b113c72d270d161c1eaa20
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-19 16:07:36 +0100

    feat(components): improved buttons for accessibility
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- components/button.tsx
+++ components/button.tsx
@@ -18,11 +18,15 @@ export default function Button({
 }: Props) {
   return (
     <a
-      className={`group border-foreground/10 bg-foreground/85 text-background hover:bg-foreground/65 flex h-12 w-full items-center justify-center gap-4 overflow-hidden rounded-full border px-5 font-semibold transition duration-300 sm:flex-1/3 md:w-39.5 ${disabled ? "pointer-events-none cursor-not-allowed opacity-50" : "cursor-pointer"}`}
+      role="button"
       href={url}
-      aria-disabled={disabled}
       target="_blank"
       rel="noopener noreferrer"
+      aria-disabled={disabled}
+      aria-label={alt}
+      aria-pressed="false"
+      tabIndex={0}
+      className={`group border-foreground/10 bg-foreground/85 text-background hover:bg-foreground/65 flex h-12 w-full items-center justify-center gap-4 overflow-hidden rounded-full border px-5 font-semibold transition duration-300 sm:flex-1/3 md:w-39.5 ${disabled ? "pointer-events-none cursor-not-allowed opacity-50" : "cursor-pointer"}`}
     >
       <Image
         loading="lazy"
