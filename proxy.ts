9bcaa34625c3b8338214641f02a52479ff20f9fa
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-19 14:57:05 +0100

    feat(proxy): set CORS headers
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,50 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
+// CORS Config
+const allowedOrigins = [
+  "https://ava.radobley.com",
+  "https://www.radobley.sk",
+  "https://www.radobley.com",
+];
+
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export function proxy(request: NextRequest) {
+  // Check origin from the request
+  const origin = request.headers.get("origin") ?? "";
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  // Handle preflighted requests
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  return response;
+}
+
+// i18n config
 export default createMiddleware(routing);
 
 export const config = {
