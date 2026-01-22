6ecc0525751591d1deaefd3edb708c0121fa81d5
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-22 14:54:46 +0100

    feat(homepage): improved mobile layout
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- components/button.tsx
+++ components/button.tsx
@@ -23,7 +23,6 @@ export default function Button({
       target="_blank"
       rel="noopener noreferrer"
       aria-disabled={disabled}
-      aria-label={alt}
       aria-pressed="false"
       tabIndex={0}
       className={`group border-foreground/10 bg-foreground/85 text-background hover:bg-foreground/65 flex h-12 w-full items-center justify-center gap-4 overflow-hidden rounded-full border px-5 font-semibold transition duration-300 sm:flex-1/3 md:w-39.5 ${disabled ? "pointer-events-none cursor-not-allowed opacity-50" : "cursor-pointer"}`}
