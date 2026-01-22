3cba3653b2377b86781b8d8f6b2d4f44b769337e
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-22 20:43:50 +0100

    fix(components): not centered text in feature blocks 1 and 2
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- components/FeatureBlock1.tsx
+++ components/FeatureBlock1.tsx
@@ -8,7 +8,7 @@ export default function FeatureBlock1() {
   const t = useTranslations("homepage.features.feature1");
 
   return (
-    <div className="grid grid-cols-1 place-items-center gap-y-12 sm:grid-cols-2">
+    <div className="grid grid-cols-1 place-items-center gap-y-12 text-center sm:grid-cols-2 sm:text-left">
       <div className="order-1 flex flex-col gap-10">
         <h2 className="text-3xl font-semibold tracking-tight">
           {t("headline")}
--- components/FeatureBlock2.tsx
+++ components/FeatureBlock2.tsx
@@ -8,7 +8,7 @@ export default function FeatureBlock2() {
   const t = useTranslations("homepage.features.feature2");
 
   return (
-    <div className="grid w-full grid-cols-1 place-items-center gap-y-12 sm:grid-cols-2">
+    <div className="grid w-full grid-cols-1 place-items-center gap-y-12 text-center sm:grid-cols-2 sm:text-left">
       <div className="order-1 flex flex-col gap-10 sm:order-2">
         <h2 className="text-3xl font-semibold tracking-tight">
           {t("headline")}
